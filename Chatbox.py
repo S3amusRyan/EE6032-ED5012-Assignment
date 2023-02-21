ed22626dd6ce71c433504439c80a86489db354e9
S3amusRyan <125382510+S3amusRyan@users.noreply.github.com>
2023-02-21 15:55:40 +0000

    Update Chatbox.py

--- Chatbox.py
+++ Chatbox.py
@@ -22,8 +22,7 @@ def send():
 
 	e.delete(0, END)
 
-
-lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10, width=20, height=1).grid(
+lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="A", font=FONT_BOLD, pady=10, width=20, height=1).grid(
 	row=0)
 
 txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
